dd49fa7be7157762406e386796bcb48048b4b0a8
Norbert Benczúr <norbert.benczur@gmail.com>
2026-04-14 20:45:26 +0200

    Load the texture into a ByteBuffer

--- src/main/java/com/github/nukesz/Texture.java
+++ src/main/java/com/github/nukesz/Texture.java
@@ -1,10 +1,22 @@
 package com.github.nukesz;
 
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.stb.STBImage.stbi_load;
+
 public class Texture {
 
-    private final String path;
+    private static final int NUMBER_OF_COLOR_CHANNELS = 4;
 
     public Texture(String path) {
-        this.path = path;
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            IntBuffer width = stack.mallocInt(1);
+            IntBuffer height = stack.mallocInt(1);
+            IntBuffer channels = stack.mallocInt(1);
+            ByteBuffer buffer = stbi_load(path, width, height, channels, NUMBER_OF_COLOR_CHANNELS);
+        }
     }
 }
